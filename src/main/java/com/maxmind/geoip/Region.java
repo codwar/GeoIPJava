9c7f3cee45ff39cedbe3ac0e103b1ec55db39e7e
dahool <gabriel.sgt@gmail.com>
2012-03-31 12:06:09 -0300

    initial commit

---
+++ src/main/java/com/maxmind/geoip/Region.java
@@ -0,0 +1,32 @@
+package com.maxmind.geoip;
+
+public class Region {
+	private String countryCode;
+	private String countryName;
+	private String region;
+
+	public String getCountryCode() {
+		return countryCode;
+	}
+
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+
+	public String getCountryName() {
+		return countryName;
+	}
+
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+}
